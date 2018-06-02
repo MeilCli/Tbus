e3e39f0ca3cf47dd9ef31f68f6149b0239313e1e
meil <ics.sora.meil@gmail.com>
2018-06-02 17:33:08 +0900

    make easy page

--- Tbus.Web/app.ts
+++ Tbus.Web/app.ts
@@ -3,23 +3,39 @@
 
 class Greeter {
     element: HTMLElement;
-    span: HTMLElement;
+    span1: HTMLElement;
+    span2: HTMLElement;
+    span3: HTMLElement;
+    span4: HTMLElement;
     timerToken: number;
 
     constructor(element: HTMLElement) {
         this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        // this.span.innerText = new Date().toUTCString();
+        this.span1 = document.createElement('p');
+        this.element.appendChild(this.span1);
+        this.span2 = document.createElement('p');
+        this.element.appendChild(this.span2);
+        this.span3 = document.createElement('p');
+        this.element.appendChild(this.span3);
+        this.span4 = document.createElement('p');
+        this.element.appendChild(this.span4);
     }
 
     async s() {
-        this.span.innerHTML = await new Tbus.Web.TbusManager().get3Bus()
+        var manager = new Tbus.Web.TbusManager();
+        var bus1 = await manager.getNextBusAsync("kansai_takatuki");
+        this.span1.innerHTML = `関西大学発 ${bus1.hour}:${bus1.minute} 行先: ${bus1.destination}`;
+        var bus2 = await manager.getNextBusAsync("kansai_tonda");
+        this.span2.innerHTML = `関西大学発 ${bus2.hour}:${bus2.minute} 行先: ${bus2.destination}`;
+        var bus3 = await manager.getNextBusAsync("takatuki_kansai");
+        this.span3.innerHTML = `JR高槻駅北発 ${bus3.hour}:${bus3.minute} 行先: ${bus3.destination}`;
+        var bus4 = await manager.getNextBusAsync("tonda_kansai");
+        this.span4.innerHTML = `JR富田駅発 ${bus4.hour}:${bus4.minute} 行先: ${bus4.destination}`;
     }
 
     start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+        // タイマー使うことになればこれを使おう
+        this.timerToken = setInterval(() => this.span1.innerHTML = new Date().toUTCString(), 500);
     }
 
     stop() {
--- Tbus.Web/src/TbusManager.ts
+++ Tbus.Web/src/TbusManager.ts
@@ -31,20 +31,110 @@ namespace Tbus.Web {
         weekday_table: DayTable
         saturday_table: DayTable
         sunday_table: DayTable
-        special_days: { key: Date; value: DayTable }
+        special_days: { [key: string]: DayTable; }
     }
 
     export class TbusManager {
         private httpClient = new HttpClient();
+        private host = "https://meilcli.github.io/Tbus/";
 
-        async get3Bus(): Promise<string> {
-            var httpRequest = new HttpRequest("kansai_takatuki.limited1.json");
+        async getNextBusAsync(fileName: string): Promise<Bus> {
+            var dayTime = await this.getTodayTableAsync(fileName);
+
+            var result: Bus = null;
+            var date = new Date();
+            var time = date.getHours() * 100 + date.getMinutes();
+            for (var i = 0; i < dayTime.buses.length; i++) {
+                var bus = dayTime.buses[i];
+                if (bus.hour * 100 + bus.minute < time) {
+                    continue;
+                }
+                result = bus;
+                break;
+            }
+
+            return result;
+        }
+
+        async getTodayTableAsync(fileName: string): Promise<DayTable> {
+            var defaultTimeTable = await this.getTimeTableAsync(`${this.host}${fileName}.json`);
+            var limitedTimeTable: TimeTable[] = [];
+            for (var i = 0; i < 10; i++) {
+                // 10ぐらいで妥当なはず
+                var timeTable = await this.getTimeTableAsync(`${this.host}${fileName}.limited${i + 1}.json`);
+                if (timeTable == null) {
+                    break;
+                }
+                limitedTimeTable[i] = timeTable;
+            }
+
+            return this.getTodayTimeTable(defaultTimeTable, limitedTimeTable);
+        }
+
+        private getTodayTimeTable(defaultTimeTable: TimeTable, limitedTimeTable: TimeTable[]): DayTable {
+            var date = new Date();
+            for (var i = 0; i < limitedTimeTable.length; i++) {
+                var iso = date.toISOString()
+                var dateString = `${iso.substring(0, iso.indexOf("T"))}T00:00:00+09:00`;
+                var dayTime = limitedTimeTable[i].special_days[dateString];
+                if (dayTime != null) {
+                    return dayTime;
+                }
+            }
+
+            for (var i = 0; i < limitedTimeTable.length; i++) {
+                if (this.getDayTime(date) < this.getDayTime(limitedTimeTable[i].limited_time_option.start_day)) {
+                    // 期間前
+                    continue;
+                }
+                if (this.getDayTime(limitedTimeTable[i].limited_time_option.end_day) < this.getDayTime(date)) {
+                    // 期間後
+                    continue;
+                }
+
+                var dayTable: DayTable
+                if (date.getDay() == 0) {
+                    // 日曜日
+                    dayTable = limitedTimeTable[i].sunday_table;
+                } else if (date.getDay() == 6) {
+                    // 土曜日
+                    dayTable = limitedTimeTable[i].saturday_table;
+                } else {
+                    dayTable = limitedTimeTable[i].weekday_table;
+                }
+                if (dayTable != null) {
+                    return dayTable;
+                }
+            }
+
+            if (date.getDay() == 0) {
+                // 日曜日
+                return defaultTimeTable.sunday_table;
+            }
+            if (date.getDay() == 6) {
+                // 土曜日
+                return defaultTimeTable.saturday_table;
+            }
+            return defaultTimeTable.weekday_table;
+        }
+
+        private async getTimeTableAsync(url: string): Promise<TimeTable> {
+            var httpRequest = new HttpRequest(url);
             var response = await this.httpClient.getAsync(httpRequest);
             if (response.status != 200) {
-                return "error";
+                return null;
             }
-            var timeTable: TimeTable = JSON.parse(response.content);
-            return timeTable.limited_time_option.start_day.getDay().toString();
+            var timeTable: TimeTable = JSON.parse(response.content, (key, value) => {
+                if (typeof (value) == "string" && value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}$/)) {
+                    return new Date(Date.parse(value as string));
+                }
+                return value;
+            });
+            return timeTable;
+        }
+
+        private getDayTime(date: Date): number {
+            return date.getFullYear() * 10000 + date.getMonth() * 100 + date.getDate();
         }
     }
 }
