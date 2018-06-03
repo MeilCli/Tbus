af61d9d063d3de814dfc8bc9dc4736a5adc8b08f
meil <ics.sora.meil@gmail.com>
2018-06-03 13:01:39 +0900

    add timetable resource

---
+++ Tbus.App.NETStandard/Constants/TimeTableFileConstant.cs
@@ -0,0 +1,25 @@
+﻿namespace Tbus.App.NETStandard.Constants
+{
+    internal class TimeTableFileConstant
+    {
+        public static readonly string[] KansaiToTakatuki = new[]
+        {
+            "kansai_takatuki.json","kansai_takatuki.limited1.json","kansai_takatuki.limited2.json"
+        };
+
+        public static readonly string[] KansaiToTonda = new[]
+        {
+            "kansai_tonda.json","kansai_tonda.limited1.json","kansai_tonda.limited2.json"
+        };
+
+        public static readonly string[] TakatukiToKansai = new[]
+        {
+            "takatuki_kansai.json","takatuki_kansai.limited1.json","takatuki_kansai.limited2.json"
+        };
+
+        public static readonly string[] TondaToKansai = new[]
+        {
+            "tonda_kansai.json","tonda_kansai.limited1.json","tonda_kansai.limited2.json"
+        };
+    }
+}
