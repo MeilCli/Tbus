0d3744feb4787de6ab0e611419974c6cb27e9c65
meil <ics.sora.meil@gmail.com>
2018-06-03 12:32:38 +0900

    add app project and enable latest C# in all build of all project

---
+++ Tbus.App.XamarinForms/Tbus.App.XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tbus.App.XamarinForms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
